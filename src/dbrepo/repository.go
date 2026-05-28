96e572606c283262094f4a78ecb2ce7b53c41c46
Rha02 <romuzus08@gmail.com>
2026-05-27 20:50:33 -0500

    Simple search implemented

--- src/dbrepo/repository.go
+++ src/dbrepo/repository.go
@@ -4,4 +4,5 @@ import "github.com/Rha02/symfonia-backend/src/models"
 
 type DatabaseRepository interface {
 	GetStocks(limit int, skip int) (*[]models.Stock, error)
+	SearchStock(searchKey string) (*[]models.Stock, error)
 }
